ff0cda48af3b96e0ed55d15a80f62e3272dd45cc
Prad Nukala <pradn@sonr.io>
2022-08-19 15:20:05 -0400

    Configure Nextra with existing docs

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
   return (
@@ -14,59 +14,63 @@ const Home: NextPage = () => {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to the <a href="https://nextjs.org">Sonr Docs!</a>
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.tsx</code>
+          The Universal SDK is a set of libraries and tools that allow
+          developers to easily interact with Sonr&pos;s blockchain.
         </p>
 
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
+          <a href="/build" className={styles.card}>
+            <h2>Build &rarr;</h2>
             <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
+              Build user-centric, and privacy preserving App&pos;s with the
+              Highway stack.
+            </p>
+          </a>
+
+          <a href="/design" className={styles.card}>
+            <h2>Design &rarr;</h2>
+            <p>
+              Incorporate the Nebula Design System for a seamless user
+              experience!
+            </p>
+          </a>
+
+          <a href="/integrate" className={styles.card}>
+            <h2>Integrate &rarr;</h2>
+            <p>
+              Use your Highway powered application on Swift, React Native,
+              Flutter and more!
+            </p>
+          </a>
+
+          <a href="/architecture" className={styles.card}>
+            <h2>Reference &rarr;</h2>
+            <p>
+              In depth technical documentation about the Sonr Blockchain
+              modules.
             </p>
           </a>
         </div>
       </main>
 
       <footer className={styles.footer}>
-        <a
+        {/* <a
           href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
+          Powered by{" "}
           <span className={styles.logo}>
             <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
           </span>
-        </a>
+        </a> */}
       </footer>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
