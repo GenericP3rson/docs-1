5c0d67b7f16baaa659caaa57102d241176a12747
brokecollegedevwithaclothingobsession <supremehitsheads@gmail.com>
2022-08-31 16:05:39 -0700

    removal of dist

--- .vitebook/dist/assets/js/cli-overview.0d03bdd7.js
+++
@@ -1,9 +0,0 @@
-import{S as c,b as r,s as d,W as h,q as i,X as o,l as v,n as s,j as m}from"./framework.40d14ebd.js";function p(_){let a,n=`<h1 id="speedway-cli-overview" tabindex="-1" class="__vbk__"><a class="header-anchor __vbk__" href="#speedway-cli-overview" aria-hidden="true">#</a> Speedway CLI Overview</h1>
-<p class="__vbk__">OVERVIEW</p>
-<h2 id="cli-installation" tabindex="-1" class="__vbk__"><a class="header-anchor __vbk__" href="#cli-installation" aria-hidden="true">#</a> CLI Installation</h2>
-<p class="__vbk__">INSTALLATION LINK</p>
-<h2 id="cli-commands" tabindex="-1" class="__vbk__"><a class="header-anchor __vbk__" href="#cli-commands" aria-hidden="true">#</a> CLI Commands</h2>
-<p class="__vbk__">COMMAND REFERENCE LINK</p>
-<h2 id="cli-examples" tabindex="-1" class="__vbk__"><a class="header-anchor __vbk__" href="#cli-examples" aria-hidden="true">#</a> CLI Examples</h2>
-<p class="__vbk__">Create a new account</p>
-<p class="__vbk__">Login to an account</p>`,l;return{c(){a=new h(!1),l=i(),this.h()},l(e){a=o(e,!1),l=i(),this.h()},h(){a.a=l},m(e,t){a.m(n,e,t),v(e,l,t)},p:s,i:s,o:s,d(e){e&&m(l),e&&a.d()}}}const u={excerpt:"",headers:[{level:2,title:"CLI Installation",slug:"cli-installation",children:[]},{level:2,title:"CLI Commands",slug:"cli-commands",children:[]},{level:2,title:"CLI Examples",slug:"cli-examples",children:[]}],title:"CLI Overview",frontmatter:{title:"CLI Overview"},lastUpdated:1661875050472};class C extends c{constructor(a){super(),r(this,a,null,p,d,{})}}export{u as __pageMeta,C as default};
