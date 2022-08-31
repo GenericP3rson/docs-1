525f5bdf849acdbf0c12bc31831f814423e697c7
mcjcloud <brayden14cloud@gmail.com>
2022-08-31 16:59:54 -0500

    remove dist

--- .vitebook/dist/assets/js/overview.977cf1c5.js
+++
@@ -1,6 +0,0 @@
-import{S as l,b as _,s as d,W as p,q as o,X as h,l as c,n as r,j as v}from"./framework.40d14ebd.js";function u(n){let e,i=`<h1 id="overview" tabindex="-1" class="__vbk__"><a class="header-anchor __vbk__" href="#overview" aria-hidden="true">#</a> Overview</h1>
-<p class="__vbk__">The Motor SDK is a set of developer tools for building decentralized applications on the Sonr network. It currently supports Desktop, Android, and Flutter targets with plans to support iOS, React Native, and Web in the near future.</p>
-<p class="__vbk__">Hop into a target to get started!</p>
-<ul class="__vbk__">
-<li class="__vbk__"><a href="https://docs.sonr.io/motor-sdk/android/overview.html" target="_blank" rel="noopener noreferrer" class="__vbk__">Android<OutboundLink></OutboundLink></a></li>
-</ul>`,a;return{c(){e=new p(!1),a=o(),this.h()},l(t){e=h(t,!1),a=o(),this.h()},h(){e.a=a},m(t,s){e.m(i,t,s),c(t,a,s)},p:r,i:r,o:r,d(t){t&&v(a),t&&e.d()}}}const m={excerpt:"",headers:[],title:"Overview",frontmatter:{},lastUpdated:1661982973045};class b extends l{constructor(e){super(),_(this,e,null,u,d,{})}}export{m as __pageMeta,b as default};
