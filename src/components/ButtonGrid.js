72302f856639c0a7caf20b937f01645d5bb5ae2f
Prad Nukala <pradn@sonr.io>
2022-09-06 14:22:28 -0500

    Add overview grid buttons

--- src/components/ButtonGrid.js
+++ src/components/ButtonGrid.js
@@ -16,7 +16,7 @@ function RootButtonGrid({ children, color }) {
                             alignItems="center"
                         >
                             <Grid item xs={2} md={2}>
-                                <img src="/img/roadmap.png" />
+                                <img src="/3d/1-roadmap.png" />
                             </Grid>
                             <Grid item>
                                 {" "}
@@ -38,7 +38,7 @@ function RootButtonGrid({ children, color }) {
                         >
                             <Grid item xs={2} md={2}>
 
-                                <img src="/img/components.png" />
+                                <img src="/3d/1-components.png" />
 
                             </Grid>
                             <Grid item>
@@ -60,7 +60,7 @@ function RootButtonGrid({ children, color }) {
                             alignItems="center"
                         >
                             <Grid item xs={2} md={2}>
-                                <img src="/img/features.png" />
+                                <img src="/3d/1-features.png" />
                             </Grid>
                             <Grid item>
                                 {" "}
@@ -81,7 +81,7 @@ function RootButtonGrid({ children, color }) {
                             alignItems="center"
                         >
                             <Grid item xs={2} md={2}>
-                                <img src="/img/advanced.png" />
+                                <img src="/3d/1-advanced.png" />
 
                             </Grid>
                             <Grid item>
@@ -102,7 +102,7 @@ function GetStartedButtonGrid({ children, color }) {
     return (
         <div class="pagination-nav buttonGridTopMargin">
             <div class="pagination-nav__item pagination-nav__item--next">
-                <a class="pagination-nav__link pagination-nav__item--next" href="">
+                <a class="pagination-nav__link pagination-nav__item--next" href="/docs/guides/overview">
                     <Box sx={{ flexGrow: 1 }}>
                         <Grid
                             container
@@ -111,56 +111,12 @@ function GetStartedButtonGrid({ children, color }) {
                             alignItems="center"
                         >
                             <Grid item xs={2} md={2}>
-                                <img src="/img/icons/icon-72x72.png" />
+                                <img src="/3d/2-overview.png" />
                             </Grid>
                             <Grid item>
                                 {" "}
-                                <div class="pagination-nav__sublabel">What we have planned</div>
-                                <div class="pagination-nav__label">Roadmap</div>
-                            </Grid>
-                        </Grid>
-                    </Box>
-                </a>
-            </div>
-            <div class="pagination-nav__item pagination-nav__item--next">
-                <a class="pagination-nav__link" href="/docs/basics/components">
-                    <Box sx={{ flexGrow: 1 }}>
-                        <Grid
-                            container
-                            spacing={2}
-                            justifyContent="flex-start"
-                            alignItems="center"
-                        >
-                            <Grid item xs={2} md={2}>
-
-                                <img src="/img/icons/icon-72x72.png" />
-
-                            </Grid>
-                            <Grid item>
-                                {" "}
-                                <div class="pagination-nav__sublabel">The Anatomy of our Architecture</div>
-                                <div class="pagination-nav__label">Components</div>
-                            </Grid>
-                        </Grid>
-                    </Box>
-                </a>
-            </div>
-            <div class="pagination-nav__item pagination-nav__item--next">
-                <a class="pagination-nav__link" href="/docs/basics/features">
-                    <Box sx={{ flexGrow: 1 }}>
-                        <Grid
-                            container
-                            spacing={2}
-                            justifyContent="flex-start"
-                            alignItems="center"
-                        >
-                            <Grid item xs={2} md={2}>
-                                <img src="/img/icons/icon-72x72.png" />
-                            </Grid>
-                            <Grid item>
-                                {" "}
-                                <div class="pagination-nav__sublabel">Everything Sonr can do!</div>
-                                <div class="pagination-nav__label">Features</div>
+                                <div class="pagination-nav__sublabel">Structure of a Sonr App</div>
+                                <div class="pagination-nav__label">Overview</div>
                             </Grid>
                         </Grid>
                     </Box>
@@ -176,13 +132,57 @@ function GetStartedButtonGrid({ children, color }) {
                             alignItems="center"
                         >
                             <Grid item xs={2} md={2}>
-                                <img src="/img/icons/icon-72x72.png" />
+
+                                <img src="/3d/2-install.png" />
 
                             </Grid>
                             <Grid item>
                                 {" "}
-                                <div class="pagination-nav__sublabel">In-Depth Technical Docs</div>
-                                <div class="pagination-nav__label">Advanced</div>
+                                <div class="pagination-nav__sublabel">Get our CLI and SDKs</div>
+                                <div class="pagination-nav__label">Installation</div>
+                            </Grid>
+                        </Grid>
+                    </Box>
+                </a>
+            </div>
+            <div class="pagination-nav__item pagination-nav__item--next">
+                <a class="pagination-nav__link" href="/blog">
+                    <Box sx={{ flexGrow: 1 }}>
+                        <Grid
+                            container
+                            spacing={2}
+                            justifyContent="flex-start"
+                            alignItems="center"
+                        >
+                            <Grid item xs={2} md={2}>
+                                <img src="/3d/2-tutorials.png" />
+                            </Grid>
+                            <Grid item>
+                                {" "}
+                                <div class="pagination-nav__sublabel">Guides for building on Sonr</div>
+                                <div class="pagination-nav__label">Tutorials</div>
+                            </Grid>
+                        </Grid>
+                    </Box>
+                </a>
+            </div>
+            <div class="pagination-nav__item pagination-nav__item--next">
+                <a class="pagination-nav__link" href="/docs/reference/speedway/cli">
+                    <Box sx={{ flexGrow: 1 }}>
+                        <Grid
+                            container
+                            spacing={2}
+                            justifyContent="flex-start"
+                            alignItems="center"
+                        >
+                            <Grid item xs={2} md={2}>
+                                <img src="/3d/2-reference.png" />
+
+                            </Grid>
+                            <Grid item>
+                                {" "}
+                                <div class="pagination-nav__sublabel">API Explorer and Types</div>
+                                <div class="pagination-nav__label">Reference</div>
                             </Grid>
                         </Grid>
                     </Box>
